1887601aa7508796dfdea43cdd4ee744a2538082
Tocinofrito <chichamalo@gmail.com>
2023-10-06 14:13:55 -0600

    Se añade consulta a rutas y nodemon para no tener que estar cerrando y abriendo el sv

--- cat_Estados.js
+++ cat_Estados.js
@@ -1,6 +1,14 @@
+//Importamos express y el gestor de conexión a postgresql
+const express = require('express');
 const { Client } = require('pg');
 const fs = require('fs');
+//Usamos express para el servidor
+const app = express();
+const port = 3000;
 
+//Middleware para manejar solicitudes JSON
+app.use(express.json());
+//Así procesa automaticamente los datos JSON que se requieren al llamar a
 //Leemos archivo JSON para configuración dela base de datos
 fs.readFile('config.json', 'utf8', (err, data) =>{
     if (err) {
@@ -8,22 +16,55 @@ fs.readFile('config.json', 'utf8', (err, data) =>{
         return;
     }
     const config = JSON.parse(data);
-
+    console.log(config.user);
     //Conexión a la base de datos
     const client = new Client({
         user: config.user,
         host: config.host,
         database: config.database,
-        password: config.database,
+        password: config.password,
         port: config.port,
     });
     //Conectando a base de datos
-    client.connect()
-    .then(() => {
-        console.log('Se conectó correctamente a la base de datos');
-        //Consultas y más aquí
+    client.connect((err) =>{
+        if (err){
+            console.error('Error al conectar a la base de datos:', err);
+            return;
+        }
+        console.log('Conexión lograda a la bd');
     })
-    .catch((err) =>{
-        console.error('Error al conectar a la base de datos:', err);
-    });
+    app.get('/consulta',(req, res) =>{
+        //Obtenemos parámetro de la url cuando no hay parámetro
+        
+        const {id} = req.query;
+        if(!id){
+            const query = "SELECT * FROM cat_estados";
+        client.query(query, (err, result) =>{
+            if(err){
+                console.error('Error en la consulta:', err);
+                res.status(500).json({error:'Error en la consulta'});
+            }else{
+                //Devolvemos los resultados como JSON
+                res.json(result.rows)
+            }
+        })    
+        }
+        //Realizamos la consulta cuando hay parámetro id
+
+        const query = "SELECT * FROM cat_estados WHERE id_estado= $1";
+        client.query(query, [id], (err, result) =>{
+            if(err){
+                console.error('Error en la consulta:', err);
+                res.status(500).json({error:'Error en la consulta'});
+            }else{
+                //Devolvemos los resultados como JSON
+                res.json(result.rows)
+            }
+        })
+    })
 });
+
+//Servidor Express
+app.listen(port, () =>{
+    console.log('Servidor Express escuchando en el puert ${port}');
+})
