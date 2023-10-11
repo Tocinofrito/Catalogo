418444ef5a6a89ea8e51fe2862ea0a37c028fc58
Tocinofrito <chichamalo@gmail.com>
2023-10-11 11:07:53 -0600

    Agregado jwt y tipos de consulta en parámetro

--- cat_Estados.js
+++ cat_Estados.js
@@ -1,74 +1,79 @@
-//Importamos express y el gestor de conexión a postgresql
 const express = require('express');
 const { Client } = require('pg');
 const fs = require('fs');
-//Usamos express para el servidor
+const wt = require('jsonwebtoken');
+
 const app = express();
 const port = 3000;
 
-//Middleware para manejar solicitudes JSON
-app.use(express.json());
-//Así procesa automaticamente los datos JSON que se requieren al llamar a
-//Leemos archivo JSON para configuración dela base de datos
-fs.readFile('config.json', 'utf8', (err, data) =>{
-    if (err) {
-        console.error('Error al leer el archivo de configuración:', err);
-        return;
-    }
-    const config = JSON.parse(data);
-    console.log(config.user);
-    //Conexión a la base de datos
-    const client = new Client({
-        user: config.user,
-        host: config.host,
-        database: config.database,
-        password: config.password,
-        port: config.port,
-    });
-    //Conectando a base de datos
-    client.connect((err) =>{
-        if (err){
-            console.error('Error al conectar a la base de datos:', err);
-            return;
-        }
-        console.log('Conexión lograda a la bd');
-    })
-    app.get('/consulta/:tipo/:id?',(req, res) =>{
-        const tipo = req.params.tipo;
-        const id = req.params.id; 
-        //Comprobamos tipo de consulta 1 y retornamos todos los datos en la bd
-        if(id === '1') {
-            const query = "SELECT * FROM cat_estados";
-            client.query(query, (err, result) =>{
-                if(err){
-                console.error('Error en la consulta:', err);
-                    res.status(500).json({error:'Error en la consulta'});
-                }else{
-                 //Devolvemos los resultados como JSON
-                    res.json(result.rows)
-                }  
-            })
-        }else if(tipo === '2' && id){
-                
-        
-        
-         //Realizamos la consulta cuando hay parámetro id
+let client;
+const configData = fs.readFileSync('config.json', 'utf8'); // Leer el archivo de configuración de forma síncrona
 
-        const query = "SELECT * FROM cat_estados WHERE id_estado= $1";
-            client.query(query, [id], (err, result) =>{
-                if(err){
-                    console.error('Error en la consulta:', err);
-                    res.status(500).json({error:'Error en la consulta'});
-                }else{
-                 //Devolvemos los resultados como JSON
-                    res.json(result.rows)
-                }
-            })
-        }
-    })
+if (!configData) {
+    console.error('Error al leer el archivo de configuración');
+    process.exit(1);
+}
+
+const config = JSON.parse(configData); // Parsear los datos del archivo
+
+// Conectar a la base de datos
+client = new Client({
+    user: config.user,
+    host: config.host,
+    database: config.database,
+    password: config.password,
+    port: config.port,
 });
 
-//Servidor Express
-app.listen(port, () =>{
-    console.log('Servidor Express escuchando en el puert ${port}');
-})
+client.connect((err) => {
+    if (err) {
+    console.error('Error al conectar a la base de datos:', err);
+    process.exit(1);
+    }
+    console.log('Conectado a la base de datos');
+});
+
+app.get('/consulta/:data?', (req, res) => {
+  const SecretKey = config.secretKey; // Utilizar la clave secreta desde el archivo de configuración
+    const token = req.params.data;
+
+    if (!token) {
+    return res.status(401).send('Token JWT no proporcionado');
+    }
+
+    wt.verify(token, SecretKey, (err, decoded) => {
+    if (err) {
+        console.error('Error al verificar token:', err);
+        return res.status(401).send('Token JWT no válido');
+    }
+    const tipo = decoded.tipo;
+    const id = decoded.id;
+
+    // Realizar consultas a la base de datos aquí
+    if (tipo === '1') {
+      const query = "SELECT * FROM cat_estados";
+        client.query(query, (err, result) => {
+        if (err) {
+            console.error('Error en la consulta: ', err);
+            res.status(500).json({ error: 'Error en la consulta' });
+        } else {
+            res.json(result.rows);
+        }
+        });
+    } else if (tipo === '2' && id) {
+      const query = "SELECT * FROM cat_estados WHERE id_estado = $1";
+        client.query(query, [id], (err, result) => {
+        if (err) {
+            console.error('Error en la consulta: ', err);
+            res.status(500).json({ error: 'Error en la consulta' });
+        } else {
+            res.json(result.rows);
+        }
+        });
+    }
+    });
+});
+
+app.listen(port, () => {
+    console.log('Iniciado microservicio puerto: ', port);
+});
